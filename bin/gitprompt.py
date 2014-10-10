0a8b3fe6fcf1c77384759de15fbb813f36e1703d
Amory Meltzer <Amorymeltzer@gmail.com>
2014-10-09 17:20:07 -0700

    attribution to mnagel/clustergit

--- bin/gitprompt.py
+++ bin/gitprompt.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+# Report on git status of a given folder.  Faster than some bash-based options
+# Borrows HEAVILY from mnagel's excellent clustergit (Github: mnagel/clustergit)
 
 # Grab some libraries
 import re
