085cefdf107d2184ca003d43428b66063c75e802
Amory Meltzer <Amorymeltzer@gmail.com>
2014-10-13 10:15:51 -0700

    Cleanup directory via symlinked in home directory

--- bin/terf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for year in {1..43}
-do
-    echo -e "$year\t"
-done
--- bin/ticker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-quote=`echo $1`;
-#curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=$quote&f=l1";
-curl -s "http://finance.yahoo.com/q/hp?s=$quote+Historical+Prices" | perl -ne 'print "$1\n" if /tabledata1.*?tabledata1.*?tabledata1.*?tabledata1.*?tabledata1.*?align\=\"right\"\>(\d+,?\d*\.\d\d)/;'
