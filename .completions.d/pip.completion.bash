58075372c0670ed65d9551309845268cd4192f0b
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-04 09:44:07 -0800

    Better pip completion from http://git.io/EAsF0w, still very slow

--- .completions.d/pip.completion.bash
+++ .completions.d/pip.completion.bash
@@ -1,9 +1,29 @@
-# pip bash completion start
-_pip_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   PIP_AUTO_COMPLETE=1 $1 ) )
+
+_pip() {
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    first="${COMP_WORDS[0]}"
+
+    commands=$($first --help | awk '/Commands\:/,/General Options\:/' | \
+               \grep -E -o "^\s{2}\w+" | tr -d ' ')
+    opts=$($first --help | \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+
+
+    if [ $COMP_CWORD == 1 ] ; then
+        COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ ${cur} == -* ]] ; then
+        local command_opts=$($first $prev --help | \
+                             \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
+        COMPREPLY=( $(compgen -W "${command_opts}" -- ${cur}) )
+        return 0
+    fi
 }
-complete -o default -F _pip_completion pip
-# pip bash completion end
+
+complete -o default -F _pip pip
+complete -o default -F _pip pip2
+complete -o default -F _pip pip3
+
+# /* vim: set filetype=sh : */
