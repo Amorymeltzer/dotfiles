08029de87fb1d84bc967755f31741fff43ed6685
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-18 16:56:37 -0800

    Remove all symlinking, just one call to actually move the files into the right location

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,19 +2,4 @@
 # bootstrap.sh by Amory Meltzer
 # Bootstrap installation of home environment
 
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.colordiffrc ~/.colordiffrc
-ln -s ~/dotfiles/.completions.d ~/.completions.d/
-ln -s ~/dotfiles/.curlrc ~/.curlrc
-ln -s ~/dotfiles/.emacs ~/.emacs
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d/
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.hushlogin ~/.hushlogin
-ln -s ~/dotfiles/.inputrc ~/.inputrc
-ln -s ~/dotfiles/.perlcriticrc ~/.perlcriticrc
-ln -s ~/dotfiles/.perltidyrc ~/.perltidyrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.wgetrc ~/.wgetrc
-ln -s ~/dotfiles/.z.sh ~/.z.sh
-ln -s ~/dotfiles/bin ~/bin/
+mv ../../dotfiles/* ~/
