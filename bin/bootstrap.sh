72d2df475c6796de43eff6fb4bebdb61e5c5e0f1
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-21 08:24:53 -0800

    Roadmap eventual plan

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,4 +2,13 @@
 # bootstrap.sh by Amory Meltzer
 # Bootstrap installation of home environment
 
+
+# Download dotfiles repo
+## Use either curl or wget depending
+# Extract, move, source
+# Somehow install macports?
+# Homebrew
+# Casks - require perms?
+
+
 mv ../../dotfiles/* ~/
