ee3cee45c07a6a5cd80569481c704201b0913f42
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-23 13:06:41 -0800

    look for each file?

--- bin/tinypng.rb
+++ bin/tinypng.rb
@@ -12,26 +12,28 @@ if ARGV[0].nil?
   exit!(1)
 end
 
-input = "large-input.png"
-output = "tiny-output.png"
+ARGV.each do
+  input = "large-input.png"
+  output = "tiny-output.png"
 
-uri = URI.parse("https://api.tinypng.com/shrink")
+  uri = URI.parse("https://api.tinypng.com/shrink")
 
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
 
-# Uncomment below if you have trouble validating our SSL certificate.
-# Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem
-# http.ca_file = File.join(File.dirname(__FILE__), "cacert.pem")
+  # Uncomment below if you have trouble validating our SSL certificate.
+  # Download cacert.pem from: http://curl.haxx.se/ca/cacert.pem
+  # http.ca_file = File.join(File.dirname(__FILE__), "cacert.pem")
 
-request = Net::HTTP::Post.new(uri.request_uri)
-request.basic_auth("api", key)
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.basic_auth("api", key)
 
-response = http.request(request, File.binread(input))
-if response.code == "201"
-  # Compression was successful, retrieve output from Location header.
-  File.binwrite(output, http.get(response["location"]).body)
-else
-  # Something went wrong! You can parse the JSON body for details.
-  puts "Compression failed"
+  response = http.request(request, File.binread(input))
+  if response.code == "201"
+    # Compression was successful, retrieve output from Location header.
+    File.binwrite(output, http.get(response["location"]).body)
+  else
+    # Something went wrong! You can parse the JSON body for details.
+    puts "Compression failed"
+  end
 end
