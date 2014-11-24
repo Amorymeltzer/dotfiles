0c8bf8f1323c9436a0ff273b67eed4fcde730a9f
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-24 14:54:10 -0800

    homebrew install script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,11 +6,9 @@
 # Download dotfiles repo
 ## Use either curl or wget depending
 # Extract, move, source
+mv ../../dotfiles/* ~/
 # Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Casks - require perms?
 # Use cask to install macports
 # Proceed...
-
-
-
-mv ../../dotfiles/* ~/
