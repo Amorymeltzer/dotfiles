9523f1ce7342abf045c929dd8160069d3abbe08e
Amory Meltzer <Amorymeltzer@gmail.com>
2014-12-04 16:01:59 -0800

    List all casks without a license on the fly

--- bin/license_helper.sh
+++ bin/license_helper.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # license_helper.sh by Amory Meltzer
 # Script to help automate license updating of hombrew-cask casks
+
+grep -i "license :unknown" /usr/local/Library/Taps/caskroom/homebrew-cask/Casks/*.rb | column -t | cut -f 9 -d '/' |cut -f 1 -d ':' | sed 's/.rb$//g'
