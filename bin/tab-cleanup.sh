a610a41f97f9d3e1f68878d1425e7d15c0414c23
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-14 12:01:07 -0800

    Script to help force me to whiddle down tabs in my browser

---
+++ bin/tab-cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# tab_cleanup.sh by Amory Meltzer
+# Encourage me to cleanup my beowser tabs, one a day
+
+# 14 Jan 2015 - 181 tabs
+today=$(date +%-j)		# %- removes padding 0 in day of year %j
+tabs=$((181+14-$today))
+
+date +"%d %b %Y"
+echo $tabs tabs required
