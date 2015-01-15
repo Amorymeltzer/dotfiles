eda7525cc20bedd34b0207b85359ab22bc144b83
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-15 14:29:47 -0800

    Cleanup unnecessary lines

--- bin/license_helper.sh
+++ bin/license_helper.sh
@@ -9,6 +9,4 @@ cd /usr/local/Library/Taps/caskroom/homebrew-cask/Casks/
 cask=$(grep -i "license :unknown" *.rb|cut -f 1 -d ':'|sed 's/.rb$//g'|shuf|head -n 1)
 
 brew cask cat $cask
-echo "Looking up homepage for $cask"
-#sleep 1
 brew cask home $cask
