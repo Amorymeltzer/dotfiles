405df1809d26be97ed51443932106bca94f775be
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-26 09:02:17 -0800

    oss license helper

---
+++ bin/oss_license_helper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# license_helper.sh by Amory Meltzer
+# Script to help automate license updating of hombrew-cask casks
+## Use dedicated list to save time?
+## Ask first?
+
+cd /usr/local/Library/Taps/caskroom/homebrew-cask/Casks/
+
+cask=$(grep -C 10 -i "github" *.rb | grep -i "license :oss" | cut -f 1 -d ':'|sed 's/.rb.*$//g'|shuf|head -n 1)   
+
+brew cask cat $cask
+brew cask home $cask
