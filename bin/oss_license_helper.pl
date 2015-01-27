f37314e2aed165ce12c2bf15f35669b8429d9757
Amory Meltzer <Amorymeltzer@gmail.com>
2015-01-27 15:40:04 -0800

    Begin prompt user for license after research

--- bin/oss_license_helper.pl
+++ bin/oss_license_helper.pl
@@ -9,10 +9,32 @@ use warnings;
 use diagnostics;
 
 my $infile = '/Users/Amory/oss_gh.txt';
+my %oss;
 
+# Build hash for later parsing, etc.
 open my $ossGH, '<', "$infile" or die $!;
 while (<$ossGH>) {
   chomp;
-  print "$_\n";
+
+  my @tmp = split /\t/;
+  $tmp[1] ||= q{};		# Original value if defined, empty if not
+
+  $oss{$tmp[0]} = $tmp[1];
 }
 close $ossGH;
+
+foreach my $key (sort keys %oss) {
+  print "$key\n";
+  system "brew cask cat $key";
+  system "brew cask home $key";
+
+  print "What is the license for this cask?\n";
+  print "oss, gpl, mit, mpl, [S]kip or [Q]uit";
+
+  my $lic = <>;
+  chomp $lic;
+
+  if ($lic =~ /^q$/i) {
+    print "asdasd\n";
+  }
+}
