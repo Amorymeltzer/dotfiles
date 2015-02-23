3d7cc19169cf8d4e6f2f127b3fdae10331e9524d
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-22 17:56:11 -0800

    Check if git repo via rev-parse --is-inside-git-tree first, then check other stuff

--- bin/gitprompt.py
+++ bin/gitprompt.py
@@ -18,42 +18,43 @@ def check(dirname):
     Check the subdirectories of a single directory.
     See if they are versioned in git and display the requested information.
     """
-    out = run('cd "%s"; LC_ALL=C git status' % dirname)
 
     messages = []
-    clean = True
-    if re.search(r'nothing to commit.?.?working directory clean.?', out):
-        messages.append("=")
-    # Check for untracked files
-    if 'Changes not staged for commit' in out:
-        messages.append("+")
-        clean = False
-    if 'Untracked files' in out:
-        messages.append("?")
-        clean = False
-    if 'Changes to be committed' in out:
-        messages.append("!")
-        clean = False
-    if 'Your branch is ahead of' in out:
-        # Output
-        # ⚡ → ↑ ↓ ↕ ○ ☿ ± ✘ ¤ « ¬ ¼ ½ ¾ × ƴ ˃ ˧ ૦ ᐅ ᗆ ᗌ ᗒ ᗘ ↀ ⇛ ⇒ ⇨ ↝ ∇ ⋕
-        #  ⌁ ⌇ ⎋ ⏆ ▶ ▷ ▸ ▹ ► ▻ ◆ ◇ ◈ ◊ ☇ ☈ ✈ ➤ ➙ ⨠ 𝆓
-        messages.append("→")
-        clean = False
+    if run('git rev-parse --is-inside-work-tree 2>/dev/null'):
+        clean = True
+        out = run('cd "%s"; LC_ALL=C git status' % dirname)
+        if re.search(r'nothing to commit.?.?working directory clean.?', out):
+            messages.append("=")
+        # Check for untracked files
+        if 'Changes not staged for commit' in out:
+            messages.append("+")
+            clean = False
+        if 'Untracked files' in out:
+            messages.append("?")
+            clean = False
+        if 'Changes to be committed' in out:
+            messages.append("!")
+            clean = False
+        if 'Your branch is ahead of' in out:
+            # Output
+            # ⚡ → ↑ ↓ ↕ ○ ☿ ± ✘ ¤ « ¬ ¼ ½ ¾ × ƴ ˃ ˧ ૦ ᐅ ᗆ ᗌ ᗒ ᗘ ↀ ⇛ ⇒ ⇨ ↝ ∇ ⋕
+            #  ⌁ ⌇ ⎋ ⏆ ▶ ▷ ▸ ▹ ► ▻ ◆ ◇ ◈ ◊ ☇ ☈ ✈ ➤ ➙ ⨠ 𝆓
+            messages.append("→")
+            clean = False
         # p = re.search("Your branch is ahead of .* by (\d+) commit", out)
         # messages.append(p.group(1))
-    # Internal regex is faster than git rev-parse --abbrev-ref HEAD
-    if 'On branch ' in out:
-        branch = re.search('^On branch (.*)\n', out)
-        messages.insert(0, ' ')
-        messages.insert(0, branch.group(1))
-
+        # Internal regex is faster than git rev-parse --abbrev-ref HEAD
+        if 'On branch ' in out:
+            branch = re.search('^On branch (.*)\n', out)
+            messages.insert(0, ' ')
+            messages.insert(0, branch.group(1))
+        run('cd ../')
     else:
         messages = ["-"]
 
+
     sys.stdout.write("".join(messages))
 
-    run('cd ../')
 
 #-------------------
 # Now, onto the main event!
