72ef713ff2413348f6794cc4ba96ddffa7800d7c
Amory Meltzer <Amorymeltzer@gmail.com>
2015-02-22 18:21:36 -0800

    Add some notes about potentially useful material

--- bin/gitprompt.py
+++ bin/gitprompt.py
@@ -3,6 +3,16 @@
 # Report on git status of a given folder.  Faster than some bash-based options
 # Borrows HEAVILY from mnagel's excellent clustergit (Github: mnagel/clustergit)
 
+
+## Potentially useful for parsing branchname, in particular if everything ends
+## up FUBARd for some reason FIXME TODO
+# # Get the short symbolic ref.
+# # If HEAD isn't a symbolic ref, get the short SHA for the latest commit
+# # Otherwise, just give up.
+# branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+# git rev-parse --short HEAD 2> /dev/null || \
+# echo '(unknown)')";
+
 # Grab some libraries
 import re
 import sys
