17b644b38142c8ddf9fd02c779e165fcc2aacbc0
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-06 13:25:51 -0700

    Monday elisp update

--- .emacs.d/site-lisp/ido-ubiquitous.el
+++ .emacs.d/site-lisp/ido-ubiquitous.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ryan C. Thompson
 ;; URL: https://github.com/DarwinAwardWinner/ido-ubiquitous
-;; Version: 3.5
+;; Version: 3.6
 ;; Created: 2011-09-01
 ;; Keywords: convenience, completion, ido
 ;; EmacsWiki: InteractivelyDoThings
@@ -70,7 +70,7 @@
 ;;
 ;;; Code:
 
-(defconst ido-ubiquitous-version "3.5"
+(defconst ido-ubiquitous-version "3.6"
   "Currently running version of ido-ubiquitous.
 
 Note that when you update ido-ubiquitous, this variable may not
@@ -815,7 +815,7 @@ If there is no override set for CMD in
                   (display-warning
                    'ido-ubiquitous
                    (format "Removing invalid override `%S' from `ido-ubiquitous-command-overrides'"
-                           (cons action spec) action)
+                           (cons action spec))
                    :warning)
                   (setq ido-ubiquitous-command-overrides
                         (remove override ido-ubiquitous-command-overrides)))
