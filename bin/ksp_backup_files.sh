349abc2dbdb0d9fe93abda82429a585557c03e74
Amory Meltzer <Amorymeltzer@gmail.com>
2015-07-29 09:55:46 -0700

    Remove alarm backup lines
    It's all within the persistent.sfs file

--- bin/ksp_backup_files.sh
+++ bin/ksp_backup_files.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # ksp_backup_files.sh by Amory Meltzer
-# Simply copy pilot and alarm files to a save directory
+# Simply copy pilot files to a save directory
 
 dateandtime=$(date +%Y.%m.%d_%H%M%S)
 folderpath='/Users/Amory/Dropbox/KSP stuff/KSP saves backups/'$dateandtime
 ksppath='/Applications/KSP_osx'
-kacpath=$ksppath'/GameData/TriggerTech/PluginData/KerbalAlarmClock'
 
 userlist=$(ls -1 $ksppath'/saves' | grep -v scenarios | grep -v training)
 
@@ -15,8 +14,5 @@ for i in $userlist
 do
     cp "$ksppath/saves/$i/persistent.sfs" "$folderpath/$i.persistent.sfs"
 done
-
-#cp "$kacpath/config.xml" "$folderpath/"
-#cp "$kacpath/Alarms-McJohn (CAREER).txt" "$folderpath/"
 
 ls -lFGh "$folderpath/"
