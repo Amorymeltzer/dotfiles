295c02bc3012fd8f28191099ccc7e869313fe58f
Amory Meltzer <Amorymeltzer@gmail.com>
2015-09-08 06:56:04 -0700

    Monday elisp update

--- .emacs.d/site-lisp/ac-slim.el
+++ .emacs.d/site-lisp/ac-slim.el
@@ -56,33 +56,42 @@
               "[ \t]*\\(ruby\\|javascript\\|coffee\\):[ \t]*"
               content-of-line))))
 
+(defun ac-slim--line-is-empty ()
+  "Return t if line is empty."
+  (s-matches-p "^[ \t]*$" (buffer-substring-no-properties
+                           (line-beginning-position)
+                           (line-end-position))))
+
 (defun ac-slim-inside-non-slim-block ()
-  "Return t if inside ruby block, coffee block. Has bug when declare ruby:
-on first line. Will fix later."
-  (save-excursion
-    (let ((current-leading-spaces (ac-slim--line-leading-spaces)) (lup t)
-          (retval nil))
-      (while lup
+  "Return t if inside ruby block, coffee block."
+  (catch 'blk
+    (save-excursion
+      (let ((min-number-of-leading-spaces (ac-slim--line-leading-spaces)))
         (if (ac-slim--line-is-block-indicator)
-            (setq retval t lup nil)
-          (if (< (ac-slim--line-leading-spaces) current-leading-spaces)
-              (setq current-leading-spaces (ac-slim--line-leading-spaces)))
+            (throw 'blk t))
+        (while (not (or (= min-number-of-leading-spaces 0)
+                        (= 1 (line-number-at-pos))))
           (forward-line -1)
-          (setq lup (not (or (= current-leading-spaces 0)
-                             (= 1 (line-number-at-pos)))))))
-      retval)))
+          (if (ac-slim--line-is-empty)
+              nil
+            (if (< (ac-slim--line-leading-spaces) min-number-of-leading-spaces)
+                (progn
+                  (setq min-number-of-leading-spaces (ac-slim--line-leading-spaces))
+                  (if (ac-slim--line-is-block-indicator)
+                      (throw 'blk t))))))))))
 
 (defun ac-slim-tag-prefix ()
   (and (not (ac-slim-inside-ruby-code))
        (not (ac-slim-inside-non-slim-block))
        (save-match-data
          (save-excursion
-           (re-search-backward "\\(^[\t ]*\\|:[\t ]*\\)\\([^ \t]*\\)" nil t)
+           (re-search-backward "\\(^[\t ]*\\|:[\t ]*\\)\\([a-zA-Z]*\\)" nil t)
            (match-beginning 2)))))
 
 (defun ac-slim-attr-prefix ()
   (and (not (ac-slim-inside-ruby-code))
        (not (ac-slim-inside-non-slim-block))
+       (not (ac-slim-attrv-prefix))
        (save-match-data
          (save-excursion
            (re-search-backward " \\(.*\\)" nil t)
@@ -91,8 +100,33 @@ on first line. Will fix later."
 (defun ac-slim-attrv-prefix ()
   (and (not (ac-slim-inside-ruby-code))
        (not (ac-slim-inside-non-slim-block))
-       (if (re-search-backward "\\w *= *[\"]\\([^\"]+[ ]\\|\\)\\(.*\\)" nil t)
-           (match-beginning 2))))
+       (let (mb2)
+         (save-excursion
+           (save-match-data
+             (if (re-search-backward
+                  "\\w *= *[\"']\\([^\"']+[ ]\\|\\)\\([^\"']*\\)"
+                  (line-beginning-position) t)
+                 (progn
+                   (setq mb2 (match-beginning 2))
+                   (setq me2 (match-end 2))))))
+         (if (and mb2 (>= me2 (point)))
+             mb2))))
+
+(defun ac-slim-class-prefix ()
+  (and (not (ac-slim-inside-ruby-code))
+       (not (ac-slim-inside-non-slim-block))
+       (save-match-data
+         (save-excursion
+           (re-search-backward "\\(^[\t ]*\\|:[\t ]*\\)\\([a-zA-Z0-9#.]*\\.\\)\\([a-zA-Z0-9]\\)" nil t)
+           (match-beginning 3)))))
+
+(defun ac-slim-id-prefix ()
+  (and (not (ac-slim-inside-ruby-code))
+       (not (ac-slim-inside-non-slim-block))
+       (save-match-data
+         (save-excursion
+           (re-search-backward "\\(^[\t ]*\\|:[\t ]*\\)\\([a-zA-Z0-9.]*#\\)\\([a-zA-Z0-9]\\)" nil t)
+           (match-beginning 3)))))
 
 (defun ac-slim-current-tag ()
   "Return current slim tag user is typing on."
@@ -113,14 +147,10 @@ on first line. Will fix later."
   :tag-prefix ac-slim-tag-prefix
   :attr-prefix ac-slim-attr-prefix
   :attrv-prefix ac-slim-attrv-prefix
+  :class-prefix ac-slim-class-prefix
+  :id-prefix ac-slim-id-prefix
   :current-tag-func ac-slim-current-tag
   :current-attr-func ac-slim-current-attr)
-(ac-html-define-ac-source "html"
-  :tag-prefix ac-html-tag-prefix
-  :attr-prefix ac-html-attr-prefix
-  :attrv-prefix ac-html-value-prefix
-  :current-tag-func ac-html-current-tag
-  :current-attr-func ac-html-current-attr)
 
 (provide 'ac-slim)
 ;;; ac-slim.el ends here
