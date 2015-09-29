6971acd66b2b2fb4e6ecec38d8e971273bc1e782
Amory Meltzer <Amorymeltzer@gmail.com>
2015-09-29 11:54:01 -0700

    Update funds

--- bin/marketupdate.sh
+++ bin/marketupdate.sh
@@ -5,9 +5,8 @@ FILES="^DJI ^IXIC ^GSPC ^TNX"
 # Only show investments if after market close or weekend
 # Based on DST, correct using Eastern time??
 if ((`date -u '+%H'` < 13)) || ((`date -u '+%u'` > 5)); then
-    FILES="VSCPX FDIKX CCPIX FGCKX $FILES"
+    FILES="FGCKX FDIKX CCPIX VSCPX VGSNX SSO VOOG QLD QQQ VFFVX RDITX $FILES"
 fi
-
 for ticker in $FILES
 do
     finance $ticker
