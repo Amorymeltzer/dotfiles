e3da33b800040f2c008f0eac8a6695e41920b537
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-02 10:48:21 -0800

    Add three writing scripts by Matt Might
    http://matt.might.net/articles/shell-scripts-for-passive-voice-weasel-words-duplicates/

---
+++ bin/writegood-lexicalillusions.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+# Matt Might: http://matt.might.net/articles/shell-scripts-for-passive-voice-weasel-words-duplicates/
+# Finds duplicate adjacent words.
+# Modified by Amory Meltzer
+
+use strict;
+use warnings;
+use diagnostics;
+
+
+my $DupCount = 0;
+
+if (!@ARGV) {
+  print "usage: dups <file> ...\n";
+  exit;
+}
+
+while (1) {
+  my $FileName = shift @ARGV;
+
+  # Exit code = number of duplicates found.
+  exit $DupCount if (!$FileName);
+
+  open FILE, $FileName or die $!;
+
+  my $LastWord = "";
+  my $LineNum = 0;
+
+  while (<FILE>) {
+    chomp;
+
+    $LineNum ++;
+
+    my @words = split (/(\W+)/);
+
+    foreach my $word (@words) {
+
+      # Skip spaces:
+      next if $word =~ /^\s*$/;
+
+      # Skip punctuation:
+      if ($word =~ /^\W+$/) {
+	$LastWord = "";
+	next;
+      }
+
+      # Found a dup?
+      if (lc($word) eq lc($LastWord)) {
+	print "$FileName:$LineNum $word\n";
+	$DupCount ++;
+      }				# Thanks to Sean Cronin for tip on case.
+
+      # Mark this as the last word:
+      $LastWord = $word;
+    }
+  }
+
+  close FILE;
+}
