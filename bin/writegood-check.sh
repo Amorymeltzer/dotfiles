b8b246c5d13eeb87793b3f29ddb3d9af6b4603f7
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-02 11:15:36 -0800

    Use bulk checker with writegood scripts, rename for easier completion

---
+++ bin/writegood-check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# writegood-check.sh by Amory Meltzer
+# Bulk-check writing
+# Should also run nom's/node's writegood
+
+echo "Weasel words:"
+writegood--weaselwords.sh $1
+echo
+echo "Passive voice:"
+writegood--passivevoice.sh $1
+echo
+echo "Lexical dups:"
+writegood--lexicalillusions.pl $1
+echo
