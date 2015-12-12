cb8b70bfc9dfac941678fd155cc1e11ddf79c947
Amory Meltzer <Amorymeltzer@gmail.com>
2015-12-12 11:45:30 -0800

    cask installed as matter-of-fact with homebrew

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,15 +7,13 @@
 # Extract, move, source
 rsync -avxz . ~
 source ~/.bash_profile
-# Homebrew
+# Homebrew and Cask
 # Check for Homebrew
 if test ! $(which brew)
 then
     echo "  Installing Homebrew for you."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-# Casks - require perms?
-brew install caskroom/cask/brew-cask
 # Use cask to install macports
 brew cask install macports
 # Install other casks, ports, brews...
