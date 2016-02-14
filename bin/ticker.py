4a1ed0aac83c8b731b794461fdb1b3d70a325967
Amory Meltzer <Amorymeltzer@gmail.com>
2016-02-14 15:01:38 -0800

    Basic functionality
    Not quite a replacement, need to deal with up/down and formatting

--- bin/ticker.py
+++ bin/ticker.py
@@ -5,35 +5,23 @@
 
 import urllib2
 import json
-import time                     # why?!
-
 import sys
+
 if len(sys.argv) <= 1:
     sys.exit(1)
-else:
-    print "hi"
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-#Stocks can be provided with just the symbol (AAPL) or exchange:symbol (NASDAQ:AAPL)
-#stocks={"MSFT","AAPL","GOOGL","AMZN","ONDK"}
+# Stocks can be provided as symbol (AAPL) or exchange:symbol (NASDAQ:AAPL)
 stocks=sys.argv[1:]
 
-from termcolor import colored
+# Nothing fancy
+class colors:
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    ENDC = '\033[0m'
+
 quote = ""
 for i in stocks:
-    quote = i + ',' + quote
-
-#  url = "http://finance.google.com/finance/info?client=ig&q=" + quote
     url = "http://finance.google.com/finance/info?client=ig&q=" + i
 
     try:
@@ -41,18 +29,14 @@ for i in stocks:
     except Exception:
         print "Ticker lookup of %s failed" % i
         continue
-    #sys.exit(1)
 
     quote = u.read()
     obj = json.loads(quote[4:-1])
 
     for ticker in obj:
         if float(ticker["c"]) < 0:
-            color = 'red'
+            color = colors.RED
         else:
-            color = 'green'
-        #print "{} {} {} | color=".format(ticker["t"], ticker["l"], ticker["c"]), "red" if float(ticker["c"]) < 0 else "green"
-        print color
-        print "{} {} {}".format(ticker["t"], ticker["l"], ticker["c"])
-        print colored ('"{} {} {}".format(ticker["t"], ticker["l"], ticker["c"])', color)
-        print bcolors.OKGREEN + "Green" + bcolors.OKBLUE + "Blue" + bcolors.ENDC
+            color = colors.GREEN
+
+        print color + "\t".join([ticker["t"], ticker["l"], ticker["c"], ticker["cp"]]) + colors.ENDC
