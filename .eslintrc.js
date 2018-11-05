389595e397e1c24054c9e688890ecf716f68c822
Amory Meltzer <Amorymeltzer@gmail.com>
2018-11-05 14:36:17 -0500

    .eslintrc.js: Create
    extend with wikimedia and prettier, for whatever that's worth
    Should probably hook this into emacs, but will need to actually use flycheck first
    http://www.flycheck.org/en/latest/languages.html#javascript

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "extends": "wikimedia",
+    "extends": ["plugin:prettier/recommended"],
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
