ee6bc7b883677bcda0fd12ea5ad7bc4af06cf8a4
Amory Meltzer <Amorymeltzer@gmail.com>
2019-08-30 20:35:23 -0400

    ksp_backup_files: Reorganize to be under profile name then date

--- bin/ksp_backup_files.sh
+++ bin/ksp_backup_files.sh
@@ -3,7 +3,7 @@
 # Simply copy pilot files to a save directory
 
 dateandtime=$(date +%Y.%m.%d_%H%M%S)
-folderpath='/Users/Amory/Documents/pilot_backups/'$dateandtime
+folderpath='/Users/Amory/Documents/pilot_backups'
 ksppath='/Applications/KSP_osx'
 
 userlist=$(ls -1 $ksppath'/saves' | grep -v scenarios | grep -v training)
@@ -12,9 +12,8 @@ mkdir "$folderpath"
 
 for i in $userlist
 do
-    mkdir "$folderpath/$i"
-    cp "$ksppath/saves/$i/persistent.sfs" "$folderpath/$i/persistent.sfs"
-    cp "$ksppath/saves/$i/persistent.loadmeta" "$folderpath/$i/persistent.loadmeta"
+    mkdir -p "$folderpath/$i/$dateandtime"
+    cp "$ksppath/saves/$i/persistent.sfs" "$folderpath/$i/$dateandtime/persistent.sfs"
+    cp "$ksppath/saves/$i/persistent.loadmeta" "$folderpath/$i/$dateandtime/persistent.loadmeta"
+    ls -lFGhR "$folderpath/$i/$dateandtime"
 done
-
-ls -lFGhR "$folderpath"
