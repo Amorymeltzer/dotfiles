0f04a646aa00b116dec047a5cfac9f727579d72d
Amory Meltzer <Amorymeltzer@gmail.com>
2020-05-26 15:09:22 -0400

    backup_z_cron.sh: Dumb script to warn and check when z.sh database is ruined
    Not sure why, but z (https://github.com/rupa/z) (1.11) has been occasionally overwriting the database.  Not sure what's triggering it, but it's annoying as hell.  Running this via cron every five minutes should at least help me avoid having to go dig out my hard backups, and possibly identify what's causing the issue.

---
+++ bin/backup_z_cron.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# backup_z_cron.sh by Amory Meltzer
+# Dumb script to back-up .z database and warn me if shit's fucked
+# Run every five minutes via cron
+# Could be simple with awk, but this is clearer and will provide better reminders
+
+z="$(wc -l < ~/.z | tr -d ' ')"
+b="$(wc -l < ~/.z.bk | tr -d ' ')"
+
+if [ $z -lt $b ]; then
+    date="$(date)"
+    # Hellow darkness my old friend...
+    /opt/local/bin/terminal-notifier -message "As of $date
+Restore from ~/.z.bk" -title "ISSUE WITH .z DATABASE"
+else
+    # Party hard
+    cp ~/.z ~/.z.bk
+fi
