24806d7c756ef0a04b4c097920601fad36d625e1
Amory Meltzer <Amorymeltzer@gmail.com>
2020-06-02 15:40:23 -0400

    Add bash_history backup script to match z backup from 0f04a64

---
+++ bin/backup_bashhistory_cron.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# See backup_z_cron.sh
+
+b="$(wc -l < ~/.bash_history | tr -d ' ')"
+h="$(wc -l < ~/.bash_history.bk | tr -d ' ')"
+
+if [ $b -lt $h ]; then
+    date="$(date)"
+    # Hellow darkness my old friend...
+    /opt/local/bin/terminal-notifier -message "As of $date
+Restore from ~/.bash_history.bk" -title "ISSUE WITH BASH HISTORY"
+else
+    # Party hard
+    cp ~/.bash_history ~/.bash_history.bk
+fi
