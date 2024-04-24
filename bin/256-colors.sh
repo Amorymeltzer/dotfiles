5ebdfd715a9859c4a536c2543192e526ed5a4350
Amory Meltzer <Amorymeltzer@gmail.com>
2024-04-24 15:01:13 -0400

    256-colors.sh: Minor improvements to formatting

--- bin/256-colors.sh
+++ bin/256-colors.sh
@@ -8,15 +8,24 @@
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
 for fgbg in 38 48 ; do #Foreground/Background
-    for color in {0..256} ; do #Colors
+
+	for color in {0..15} ; do #Basic colors
 	#Display the color
 	echo -en "\e[${fgbg};5;${color}m ${color}\t\e[0m"
-	#Display 8 colors per lines
+	#Display 8 colors per line
 	if [ $((($color + 1) % 8)) == 0 ] ; then
 	    echo #New line
 	fi
-    done
-    echo #New line
+  done
+
+  for color in {16..255} ; do #Colors
+	echo -en "\e[${fgbg};5;${color}m ${color}\t\e[0m"
+	#Display 6 colors per line, nicer pattern here
+	if [ $((($color + 3) % 6)) == 0 ] ; then
+	    echo #New line
+	fi
+  done
+  echo #New line
 done
 
 exit 0
